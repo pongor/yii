eff17627e8fe148f118c32c6089ac284dc723074
pongor <pongor_yuan@yeah.net>
2016-05-13 15:53:28 +0800

    a

---
+++ vendor/mdmsoft/yii2-admin/views/assignment/index.php
@@ -0,0 +1,54 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+/* @var $searchModel mdm\admin\models\searchs\Assignment */
+/* @var $usernameField string */
+/* @var $extraColumns string[] */
+
+$this->title = Yii::t('rbac-admin', '权限分配');
+$this->params['breadcrumbs'][] = $this->title;
+
+$columns = [
+    ['class' => 'yii\grid\SerialColumn'],
+    $usernameField,
+];
+
+if (!empty($extraColumns)) {
+    $columns = array_merge($columns, $extraColumns);
+}
+$columns[] = [
+    'class' => 'yii\grid\ActionColumn',
+    'template' => '{view}'
+];
+?>
+<div class="assignment-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?php Pjax::begin(['enablePushState'=>false,]); ?>
+    <?=
+    GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            [
+                'class' => 'yii\grid\DataColumn',
+                'attribute' => $usernameField,
+            ],
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'template'=>'{view}'
+            ],
+
+        ],
+    ]);
+    ?>
+    <?php Pjax::end(); ?>
+
+</div>
---
+++ vendor/mdmsoft/yii2-admin/views/menu/index.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+/* @var $searchModel mdm\admin\models\searchs\Menu */
+
+$this->title = Yii::t('rbac-admin', '菜单');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="menu-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]);  ?>
+
+    <p>
+        <?= Html::a(Yii::t('rbac-admin', '创建菜单'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+    <?=
+    GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'name',
+            [
+                'attribute' => 'menuParent.name',
+                'filter' => Html::activeTextInput($searchModel, 'parent_name', [
+                    'class' => 'form-control', 'id' => null
+                ]),
+                'label' => Yii::t('rbac-admin', '标签'),
+            ],
+            'route',
+            'order',
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]);
+    ?>
+<?php Pjax::end(); ?>
+
+</div>
